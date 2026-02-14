8e73fd4f8f7e493d3a886992f7324480febcf12f
30fpzs <b2029mufdulay@pshs.edu.ph>
2026-02-14 19:09:16 +0800

    FINALLY DONE yIPPIEEEE

--- script.js
+++ script.js
@@ -9,5 +9,51 @@ const DATABASE =
     { location: "Farm to Table PH HQ", price: "28-30", unit: "Pesos per crop", dist: "Contact: 0917 515 5117", keywords: ["manila", "rice"] }
 ];
 
+document.getElementById("searchBtn").addEventListener("click", function() {
 
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const status = document.getElementById("miroStatus");
+    const resultSection = document.getElementById("resultsSection");
+    const tableBody = document.getElementById("tableBody");
 
+    if(!input) return alert("Mag lagay ng produkto at lugar");
+
+    status.innerText = "Input received..";
+    resultSection.classList.add("hidden");
+
+    setTimeout(() => {
+        status.innerText = "Naghahanap sa internet...";
+
+        setTimeout(() => {
+            status.innerText = "Naghahanap sa mga lokal na supplier...";
+
+            setTimeout(() => {
+                status.innerText = "Nakahanap ng mga resulta!";
+
+                const matches = DATABASE.filter(item =>
+                    input.includes(item.keywords[0]) || input.includes(item.keywords[1])
+                );
+
+                tableBody.innerHTMl ="";
+
+                if(matches.length > 0)
+                {
+                    matches.forEach(match => {
+                        let row = `<tr>
+                            <td>${match.location}</td>
+                            <td>${match.price}<br><small>${match.unit}</small></td>
+                            <td>${match.dist}</td>
+                            </tr>`;
+                        tableBody.innerHTML += row;
+                    });
+                    resultSection.classList.remove("hidden");
+                }
+
+                else
+                {
+                    status.innerText = "Walang nahanap na merkado na malapit.";
+                }
+            }, 800);
+        }, 800);
+    }, 800);
+});
