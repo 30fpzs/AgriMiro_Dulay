c5ecffa582f347e16b3b20e84a7b911b9090e932
30fpzs <b2029mufdulay@pshs.edu.ph>
2026-02-14 19:35:04 +0800

    changed terms to tagalog

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//mock up data set :D
+//mock up data set since I do not have access to a wider and actual database
 
 const DATABASE = 
 [
-    { location: "Malolos Market", price: "20-25", unit: "Pesos per crop", dist: "2 Km", keywords: ["bulacan", "rice"] },
-    { location: "Meycauayan Hub", price: "18-20", unit: "Pesos per crop", dist: "3.6 Km", keywords: ["bulacan", "rice"] },
-    { location: "Manila Port", price: "28-30", unit: "Pesos per crop", dist: "15 Km", keywords: ["manila", "rice"] },
-    { location: "Isabela Grain Ctr", price: "15-18", unit: "Pesos per crop", dist: "120 Km", keywords: ["isabela", "corn"] },
-    { location: "Farm to Table PH HQ", price: "28-30", unit: "Pesos per crop", dist: "Contact: 0917 515 5117", keywords: ["manila", "rice"] }
+    { location: "Malolos Market", price: "20-25", unit: "Pesos per kilogram", dist: "2 Km", keywords: ["bulacan", "rice"] },
+    { location: "Meycauayan Hub", price: "18-20", unit: "Pesos per kilogram", dist: "3.6 Km", keywords: ["bulacan", "rice"] },
+    { location: "Manila Port", price: "28-30", unit: "Pesos per kilogram", dist: "15 Km", keywords: ["manila", "rice"] },
+    { location: "Isabela Grain Ctr", price: "15-18", unit: "Pesos per kilogram", dist: "120 Km", keywords: ["isabela", "corn"] },
+    { location: "Farm to Table PH HQ", price: "28-30", unit: "Pesos per kilogram", dist: "Contact: 0917 515 5117", keywords: ["manila", "rice"] }
 ];
 
 document.getElementById("searchBtn").addEventListener("click", function() {
